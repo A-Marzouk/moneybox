93e45d8a872cf29f89cfc82dd9fe7403fd418bad
Ahmed-pc <perfect_ahmed@yahoo.com>
2019-08-30 11:52:36 +0300

    Products export

---
+++ app/Exports/ProductsExport.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Exports;
+
+use App\Product;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
+
+class ProductsExport implements FromCollection, WithHeadings
+{
+    public function collection()
+    {
+        return Product::all(
+            'supplier',
+            'date',
+            'name',
+            'buy_price',
+            'id'
+        );
+    }
+
+    public function headings(): array
+    {
+        return [
+            'supplier',
+            'date',
+            'item',
+            'price',
+            'id'
+        ];
+    }
+
+    public function columnFormats(): array
+    {
+        return [
+            'A' => NumberFormat::FORMAT_NUMBER,
+        ];
+    }
+}
