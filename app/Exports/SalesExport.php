fa234e73cd6e949636d315a539f7fe2a50f7d246
Ahmed-pc <perfect_ahmed@yahoo.com>
2019-11-12 17:15:39 +0200

    change currency sales export

--- app/Exports/SalesExport.php
+++ app/Exports/SalesExport.php
@@ -5,6 +5,7 @@ namespace App\Exports;
 use App\Client;
 use App\Product;
 use App\Sale;
+use danielme85\CConverter\Currency;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
@@ -32,9 +33,23 @@ class SalesExport implements FromCollection, WithHeadings
                 $totalCosts += $cost->cost ;
             }
 
+            $currency  = new Currency();
+            $usd_rate = $currency->convert('USD', $to = 'UAH', 1 , $decimals = 4);
+            $eur_rate = $currency->convert('EUR', $to = 'UAH', 1 , $decimals = 4);
+
+            $buy_price = $product->buy_price ;
+
+            if($product->currency === 'USD'){
+                $buy_price = $product->buy_price * $usd_rate  ;
+            }
+
+            if($product->currency === 'EUR'){
+                $buy_price = $product->buy_price * $eur_rate  ;
+            }
+
             $sale['product_id'] = $product->name ;
             $sale['id'] = $product->supplier ;
-            $sale['buy_price'] = $product->buy_price . ' | ' . $product->currency ;
+            $sale['buy_price'] = $buy_price ;
             $sale['total_costs'] = $totalCosts ;
         }
         return $sales ;
